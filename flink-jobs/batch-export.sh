2d3cdd9d94f9c66f294746a5eee04509a37821d9
Emmanuel Nyachoke <achachiez@gmail.com>
2022-07-21 15:59:34 +0300

    Remove Ozone Jobs

--- flink-jobs/batch-export.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-: ${JDBC_URL?"Need to set JDBC_URL"}
-: ${JDBC_USERNAME:?"Need to set JDBC_USERNAME"}
-: ${JDBC_PASSWORD:?"Need to set JDBC_PASSWORDy"}
-: ${OUTPUT_DIR:?"Need to set OUTPUT_DIR"}
-: ${LOCATION_TAG:?"Need to set LOCATION_TAG"}
-java -jar etl-export.jar --jdbc-url $JDBC_URL --jdbc-username $JDBC_USERNAME --jdbc-password $JDBC_PASSWORD --output-dir $OUTPUT_DIR --location-tag $LOCATION_TAG
--- flink-jobs/src/main/java/net/mekomsolutions/data/pipelines/batch/BatchJob.java
+++
@@ -1,4 +0,0 @@
-package net.mekomsolutions.data.pipelines.batch;
-//TODO Split Batch job to it's own class
-public class BatchJob {
-}
