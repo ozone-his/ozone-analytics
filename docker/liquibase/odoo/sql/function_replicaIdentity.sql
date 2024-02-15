c972d5f56db522f13f71119a313942c7135fcc66
Ruhanga <41738040+Ruhanga@users.noreply.github.com>
2024-02-15 10:14:43 +0300

    HSC-218: Syntax corrections to run migration scripts on postgresql DB. (#13)

--- docker/liquibase/odoo/sql/function_replicaIdentity.sql
+++ docker/liquibase/odoo/sql/function_replicaIdentity.sql
@@ -1,17 +1,14 @@
-DROP FUNCTION IF EXISTS replicaIdentity;
+DROP FUNCTION IF EXISTS replicaIdentity(text[]);
 
-GO
-
-CREATE FUNCTION replicaIdentity(tables text []) RETURNS void AS $$
-    DECLARE m   text;
-    BEGIN
-        FOREACH m IN ARRAY tables
-        LOOP
-                EXECUTE format($fmt$
-                    ALTER TABLE %I REPLICA IDENTITY FULL;
-                $fmt$, m);
-        END LOOP;
-    END;
+CREATE OR REPLACE FUNCTION replicaIdentity(tables text[]) RETURNS void AS $$
+DECLARE
+    m text;
+BEGIN
+    FOREACH m IN ARRAY tables
+    LOOP
+        EXECUTE format($fmt$
+            ALTER TABLE %I REPLICA IDENTITY FULL;
+        $fmt$, m);
+    END LOOP;
+END;
 $$ LANGUAGE plpgsql;
-
-GO
